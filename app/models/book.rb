dee0b694cdbc082e52a56c8019345a8d763974bd
Takayuki Watanabe <watanabe@sikmi.com>
2023-08-10 11:53:23 +0900

    createするときにtraitsの内容が重複する場合は、後勝ちで上書きされる

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ApplicationRecord
+  enum status: { available: 0, borrowed: 1, lost: 2 }
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is overridden if the contents of the trait are covered.' do
+    expect(FactoryBot.create(:book, :available, :borrowed).status).to eq('borrowed')
+    expect(FactoryBot.create(:book, :borrowed, :available).status).to eq('available')
+  end
 end
